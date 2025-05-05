f0ecdb14244b041a0aa898fd39a59b3819749847
Ashraful <ashrafulmomin7076@gmail.com>
2025-05-05 18:47:35 +0530

    corecct code in backend

--- server/app.js
+++ server/app.js
@@ -14,20 +14,15 @@ import checkAuthMiddleWare from "./middleWare/checkAuthMiddleWare.js";
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-  }
-));
-
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 
 
 app.get("/", async (req, res) => {
-  console.log(req.headers)
+  console.log(req.cookies)
   try {
     let product = await ProductModel.find();
     return res.json(product);
@@ -36,6 +31,7 @@ app.get("/", async (req, res) => {
   }
 });
 app.get("/get/:id", async (req, res) => {
+  console.log(req.cookies, "user id as cookie")
   console.log(req.cookies)
   const id = req.params.id;
   try {
@@ -49,10 +45,11 @@ app.get("/get/:id", async (req, res) => {
 
 app.put("/update/:id", checkAuthMiddleWare, async (req, res) => {
   const id = req.params.id;
-  console.log(id)
+  console.log(id, "product id");
   try {
     const updated = await ProductModel.findByIdAndUpdate(id, req.body, { new: true });
-    return res.json(updated);
+    console.log(updated)
+    return res.status(200).json({ message: "Product updated successfully" });
   } catch (error) {
     return res.status(404).json({ message: "Error on updating time " });
   }
@@ -63,7 +60,7 @@ app.delete("/delete/:id", checkAuthMiddleWare, async (req, res) => {
   try {
     const deleted = await ProductModel.findByIdAndDelete(id);
     return res.json({ message: "Product deleted successfully" });
- 
+
   } catch (error) {
     return res.status(404).json({ message: "Product not found" });
   }
@@ -87,16 +84,43 @@ app.post("/auth/login", async (req, res) => {
     const user = await UserModel.findOne({ email, password });
     // if (user.role !== process.env.role) return res.status(401).json({ message: "You are not authenticated to log in. Only the Owner of the website can authenticate to log in." });
     res.cookie("token", user._id, {
-      httpOnly: true
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24)
     })
     return res.status(200).json({ message: "Login Successfully" });
   } catch (error) {
     return res.status(404).json({ message: "User not found , invalid credentials" });
   }
-})
+});
 
-app.post("logout", (req, res) => {
+app.post("/auth/logout", (req, res) => {
   res.clearCookie("token").json({ message: "Logout Successfully" });
-})
+});
+
+app.get('/auth/check', (req, res) => {
+  const token = req.cookies.token;
+  console.log(token);
+  if (!token) return res.json({ loggedIn: false });
+
+  // verify token logic
+  // res.send('hello')
+  try {
+    return res.json({ loggedIn: true });
+  } catch {
+    return res.json({ loggedIn: false });
+  }
+});
+
+
+
+app.post("/contact-us", (req, res) => {
+  // i will handle the contact us form here
+  // using nodeMailer of Resend API
+
+  res.json({ message: "message sent successfully" })
+}) 
+
+
+
 
 app.listen(3000, () => console.log("Server running on port  3000"));                                                                                                                 
--- server/middleWare/checkAuthMiddleWare.js
+++ server/middleWare/checkAuthMiddleWare.js
@@ -2,14 +2,14 @@ const checkAuthMiddleWare = async (req, res, next) => {
   console.log('hit on middleware');
   
   let token = req.cookies.token;
-  console.log(token)
+  console.log(token , "from middleware") // here the toekn accessed 
   
   if (! token) {
     return res.status(401).json({ 
       message: "You are not authenticated to log in. Only the Owner of the website can authenticate to log in." 
     });
   }
-  
+  req.user = token; // will i perse the token in every request ?
   next();
 };
 
