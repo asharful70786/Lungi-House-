9ae31034742f2dcef321fbd0b02c1f17962afecc
Ashraful <ashrafulmomin7076@gmail.com>
2025-05-08 17:44:33 +0530

    add sucurity hide info

--- server/services/CloudinaryServices.js
+++ server/services/CloudinaryServices.js
@@ -1,11 +1,12 @@
 import { v2 as cloudinary } from 'cloudinary';
 import streamifier from 'streamifier';
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: 'dvbsrbash',
-  api_key: '732986252124935',
-  api_secret: 'FtiVp0XgBm2nEzHfGEWdo8nLz20',
+  cloud_name: process.env.Cloudinary_cloud_name,
+  api_key:process.env.Cloudinary_api_key,
+  api_secret: process.env.Cloudinary_api_secret,
 });
 
 
@@ -13,7 +14,7 @@ cloudinary.config({
 export const uploadImage = (file) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { folder: 'products' }, // optional
+      { folder: 'products' }, 
       (error, result) => {
         if (error) return reject(error);
         resolve(result);
